21dbbee15d19c35b2d6025f0536183ada0ad49c4
Silvia-Du <silviadu@hotmail.it>
2022-09-26 17:13:53 +0200

    Impostato inserimento a seconda del tipo

--- Program.cs
+++ Program.cs
@@ -6,19 +6,39 @@ using System.Reflection.PortableExecutable;
 using System.Runtime.ConstrainedExecution;
 using System.Text.Json;
 
-Console.WriteLine("Crea il tuo primo programma di eventi! Inserisci il nome del programma :");
-EventsProgram newProgram = new (Console.ReadLine());
+Console.WriteLine("Crea il tuo programma di eventi! Inserisci il nome:");
+EventsProgram newProgram = new(Console.ReadLine());
+string userChoice = "";
+while(!userChoice.Contains("1") && !userChoice.Contains("2"))
+{ 
+    Console.WriteLine("Inserisci un'evento generico[1], Inserisci una Conferenza[2]");
+    userChoice = Console.ReadLine();
+
+}
+switch(userChoice)
+{
+    case "1": initProgram("Quanti eventi", "event");
+        break;
+
+    case "2": initProgram("Quante conferenze", "conference");
+        break;
+
+}
 
 
-Console.WriteLine("Quanti eventi vuoi inserire?");
-int eventsNum = Convert.ToInt32(Console.ReadLine());
 
-initProgram(eventsNum);
+//programma
+void initProgram( string type, string mood){
 
-void initProgram(int num){
+
+    Console.WriteLine($"{type} vuoi inserire?");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+
     for (int i = 0; i < num; i++)
     {
-        Event newEvent;
+        
+
         bool check = false;
         while (!check)
         {
@@ -33,12 +53,33 @@ void initProgram(int num){
                 Console.WriteLine("Inserisci il totale dei posti disponibili per l'evento");
                 int availableSeats = Convert.ToInt32(Console.ReadLine());
 
-                newEvent = new(title, date, availableSeats);
+                switch (mood)
+                {
+                    case "event":
+                        Event newEvent = newEvent = new(title, date, availableSeats);
+                        newProgram.Events.Add(newEvent);
+                        Console.WriteLine($"Il tuo evento: \n{newEvent.ToString()}");
 
-                newProgram.Events.Add(newEvent);
-                string output = newEvent.ToString();
-                Console.WriteLine($"Il tuo evento: \n{output}");
+
+                        break;
+
+                    case "conference":
+
+                        Console.WriteLine("Inserisci lo speaker dell'evento");
+                        string speaker = Console.ReadLine();
+
+                        Console.WriteLine("Inserisci il prezzo d'ingresso");
+                        double price = Convert.ToDouble(Console.ReadLine());
+
+                        Conference newConference = new(title, date, availableSeats, speaker, price);
+                        newProgram.Events.Add(newConference);
+                        Console.WriteLine($"Il tuo evento: \n{newConference.ToString()}");
+
+                        break;
+                }
+
                 check = true;
+
                 //reservation(newEvent);
                 //é la funzione che chiede se si vuole prenotare / disdire posti, l'ho commentata per testare il resto
 
@@ -62,78 +103,83 @@ DateOnly dateToCheck = DateOnly.ParseExact(Console.ReadLine(), "dd/MM/yyyy", nul
 List<Event> eventsByDate = newProgram.GetEventsByDate(dateToCheck);
 
 Console.WriteLine(EventsProgram.GetEventsList(eventsByDate));
-Console.WriteLine("Ora di addio a tutti i tuoi eventi");
-newProgram.SetEmptyList();
+//Console.WriteLine("Ora di addio a tutti i tuoi eventi");
+//newProgram.SetEmptyList();
 
 
 //gestione prenotazioni
 void reservation(Event newEvent)
 {
-    Console.WriteLine("Vuoi prenotare un posto a questo evento?[y / n]");
-    string res = Console.ReadLine();
+    string res = "";
     while (!res.Contains("y") && !res.Contains("n"))
     {
-        Console.WriteLine("risposta non valida, Vuoi prenotare dei posti per questo evento?[y / n]");
+        Console.WriteLine("Vuoi prenotare dei posti per questo evento?[y / n]");
         res = Console.ReadLine();
     }
-    if (res.Contains("y"))
+    switch (res)
     {
-        bool check = false;
-        while (!check)
-        {
-            try
+        case "y":
+            bool check = false;
+            while (!check)
             {
-                Console.WriteLine("Quanti posti vorresti Prenotare?");
-                int request = Convert.ToInt32(Console.ReadLine());
-                 string output = newEvent.setReservedSeat(request);
-                Console.WriteLine(output);
-
-                check = true;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-
-            }
-        }
-
-        bool check2 = false;
-        while (!check2)
-        {
-            try
-            {
-                Console.WriteLine("Vuoi disdire dei posti per questo evento?[y / n]");
-                string response = Console.ReadLine();
-                while (!response.Contains("y") && !response.Contains("n"))
+                try
                 {
-                    Console.WriteLine("risposta non valida, Vuoi disdire dei posti per questo evento?[y / n]");
-                    response = Console.ReadLine();
-                }
-                if (response.Contains("y"))
-                {
-                    Console.WriteLine("Quanti posti vorresti disdire?");
+                    Console.WriteLine("Quanti posti vorresti Prenotare?");
                     int request = Convert.ToInt32(Console.ReadLine());
-                    string output = newEvent.cancelReservation(request);
-                    Console.WriteLine(output);
+                    Console.WriteLine(newEvent.setReservedSeat(request));
+
+                    check = true;
                 }
-                check2 = true;
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+
+                }
             }
-            catch (Exception e)
+
+            bool check2 = false;
+            while (!check2)
             {
-                Console.WriteLine(e.Message);
+                try
+                {
+                    string response = "";
+                    while (!response.Contains("y") && !response.Contains("n"))
+                    {
+                        Console.WriteLine("Vuoi disdire dei posti per questo evento?[y / n]");
+                        response = Console.ReadLine();
+                    }
+                    switch (response)
+                    {
+                        case "y":
+                            Console.WriteLine("Quanti posti vorresti disdire?");
+                            int request = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine(newEvent.cancelReservation(request));
+                            check2 = true;
+                            break;
 
+                            case "n":
+                            Console.WriteLine("Grazie per aver utilizzato questo programma");
+                            break;
+
+                    }
+                   
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+
+                }
             }
-        }
+            break;
 
+            case "n":
+            Console.WriteLine("Grazie per aver usato il programma");
+            break;
     }
-    else if (res.Contains("n"))
-    {
-        Console.WriteLine("Grazie per aver usato il programma");
-
-    }
-
 
 }
+// fine gestione prenotazioni
+
 
 
 
